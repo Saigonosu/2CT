a4f275df39b6855f3087b5701743e97d983c0031
Ryan Devlin <saigonosu@gmail.com>
2014-06-12 19:37:23 -0400

    Format tables (terribly at first)

--- app/helpers/routes_helper.rb
+++ app/helpers/routes_helper.rb
@@ -1,2 +1,16 @@
 module RoutesHelper
+  def route_display_value(value)
+    case value
+    when Money
+      humanized_money_with_symbol(value)
+    when TrueClass, FalseClass
+      value ? 'Yes' : 'No'
+    else 
+      value
+    end
+  end
+  
+  def display_route_attribute(value)
+    value.titleize
+  end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -4,6 +4,29 @@ class Route < ActiveRecord::Base
     monetize column, with_currency: :USD
   end
 
+  def attributes_to_display
+    attrs = {}
+    %w/trip_number date advance reimbursement gross_revenue net_revenue trailer_rented trailer_rental/.each do |att|
+      attrs[att] = self.send att
+    end
+    attrs
+
+    # Blacklisting
+    # cents to money
+    # attrs = attributes.
+    #   except(*%w/id created_at updated_at/)
+    # monies = []
+    # attrs.each do |key,val| 
+    #   if att = key[/(.+)_cents/,1]
+    #     monies << att 
+    #   end
+    # end
+    # monies.each{|att| attrs[att] = self.send att}
+    # attrs.reject! do |key,val| 
+    #   key.include? '_cents' or key.include? '_currency'
+    # end
+  end
+
   def trailer_rented_str
     trailer_rented? ? 'Yes' : 'No'
   end
