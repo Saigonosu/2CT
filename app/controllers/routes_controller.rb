174c5cb9da885758edae6efa5add56d32009cd92
Ryan Devlin <saigonosu@gmail.com>
2014-06-12 17:01:19 -0400

    add trailer_rented/trailer_rental to whitelist, updated views

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -69,6 +69,6 @@ class RoutesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def route_params
-      params.require(:route).permit(:date, :trip_number, :advance, :reimbursement, :gross_revenue, :net_revenue, :driver_id,)
+      params.require(:route).permit(:date, :trip_number, :advance, :reimbursement, :gross_revenue, :net_revenue, :driver_id, :trailer_rented, :trailer_rental)
     end
 end
--- app/models/route.rb
+++ app/models/route.rb
@@ -1,7 +1,7 @@
 class Route < ActiveRecord::Base
   belongs_to :driver
   %i/advance_cents reimbursement_cents gross_revenue_cents net_revenue_cents trailer_rental_cents/.each do |column|
-    monetize column
+    monetize column, with_currency: :USD
   end
 
   def trailer_rented_str
